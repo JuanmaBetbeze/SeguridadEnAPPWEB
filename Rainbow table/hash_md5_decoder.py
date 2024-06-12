05a5ed0ffce845320ca9391ffba73e3f58dffdec
Zeke <esanchezventurini@frba.utn.edu.ar>
2024-06-12 17:49:12 -0300

    actualizacion python

--- Rainbow table/hash_md5_decoder.py
+++ Rainbow table/hash_md5_decoder.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-data = pd.read_csv(r'./md5_rainbow_table.csv')
-
 hashObjetivo = input('Ingresar hash: ')
 
-resultado = data[data['hash'] == hashObjetivo]
+for i in range(1, 12):
+    file_name = f"./RainbowTables/md5_rainbow_table_part_{i}.csv"
+    print(file_name)
+    data = pd.read_csv(file_name, dtype={"password": "string", "hash": "string"})
+    resultado = data[data['hash'] == hashObjetivo]
+    
+    if not resultado.empty:
+        break
 
 if resultado.empty:
     print('No se encontró la contraseña')
