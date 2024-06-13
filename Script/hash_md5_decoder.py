93ec7514d78eea5ab86fc5ff8d19e62226bf67ab
Zeke <esanchezventurini@frba.utn.edu.ar>
2024-06-13 19:58:44 -0300

    Cambio formato para entrega

---
+++ Script/hash_md5_decoder.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+hashObjetivo = input('Ingresar hash: ')
+
+for i in range(1, 12):
+    file_name = f"./RainbowTables/md5_rainbow_table_part_{i}.csv"
+    print('Evaluando tabla: ' + file_name)
+    data = pd.read_csv(file_name, dtype={"password": "string", "hash": "string"}, low_memory=False)
+    resultado = data[data['hash'] == hashObjetivo]
+    
+    if not resultado.empty:
+        break
+
+if resultado.empty:
+    print('No se encontró la contraseña')
+else:
+    contrasena = resultado['password'].to_string(index=False)
+    print('Contraseña encontrada: ' + contrasena)
