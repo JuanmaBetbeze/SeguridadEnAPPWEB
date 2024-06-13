6de143c7b93368e470cb07ea822d1d3e5b5476a1
Zeke <esanchezventurini@frba.utn.edu.ar>
2024-06-13 18:22:44 -0300

    saco logs y etc

--- Rainbow table/hash_md5_decoder.py
+++ Rainbow table/hash_md5_decoder.py
@@ -4,8 +4,7 @@ hashObjetivo = input('Ingresar hash: ')
 
 for i in range(1, 12):
     file_name = f"./RainbowTables/md5_rainbow_table_part_{i}.csv"
-    print(file_name)
-    data = pd.read_csv(file_name, dtype={"password": "string", "hash": "string"})
+    data = pd.read_csv(file_name, dtype={"password": "string", "hash": "string"}, low_memory=False)
     resultado = data[data['hash'] == hashObjetivo]
     
     if not resultado.empty:
@@ -14,4 +13,5 @@ for i in range(1, 12):
 if resultado.empty:
     print('No se encontró la contraseña')
 else:
-    print('Contraseña encontrada: ' + resultado.password)
+    contrasena = resultado['password'].to_string(index=False)
+    print('Contraseña encontrada: ' + contrasena)
